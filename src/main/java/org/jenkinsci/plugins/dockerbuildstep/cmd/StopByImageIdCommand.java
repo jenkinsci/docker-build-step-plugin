2ec7b9098f7580b0e7cd4fc448ab53ead700eccf
Vojtech Juranek <vjuranek@redhat.com>
2014-02-18 23:15:20 +0100

    Add Stop by image command

---
+++ src/main/java/org/jenkinsci/plugins/dockerbuildstep/cmd/StopByImageIdCommand.java
@@ -0,0 +1,53 @@
+package org.jenkinsci.plugins.dockerbuildstep.cmd;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+
+import java.util.List;
+
+import org.jenkinsci.plugins.dockerbuildstep.log.ConsoleLogger;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import com.kpelykh.docker.client.DockerClient;
+import com.kpelykh.docker.client.DockerException;
+import com.kpelykh.docker.client.model.Container;
+
+public class StopByImageIdCommand extends DockerCommand {
+
+    private String imageId;
+
+    @DataBoundConstructor
+    public StopByImageIdCommand(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    @Override
+    public void execute(@SuppressWarnings("rawtypes") AbstractBuild build, ConsoleLogger console)
+            throws DockerException {
+        if (imageId == null || imageId.isEmpty()) {
+            throw new IllegalArgumentException("At least one parameter is required");
+        }
+
+        DockerClient client = getClient();
+        List<Container> containers = getClient().listContainers(false);
+        for (Container c : containers) {
+            if (imageId.equalsIgnoreCase(c.getImage())) {
+                client.stopContainer(c.getId());
+                console.logInfo("stop container id " + c.getId());
+            }
+        }
+    }
+
+    @Extension
+    public static class StartCommandDescriptor extends DockerCommandDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Stop constainer(s) by image ID";
+        }
+    }
+
+}
