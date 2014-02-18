81689cefe779f3cb6a68e236f521e120df53c7e8
Vojtech Juranek <vjuranek@redhat.com>
2014-02-18 23:15:02 +0100

    Add Start by image command

---
+++ src/main/java/org/jenkinsci/plugins/dockerbuildstep/cmd/StartByImageIdCommand.java
@@ -0,0 +1,59 @@
+package org.jenkinsci.plugins.dockerbuildstep.cmd;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+
+import java.util.List;
+
+import org.jenkinsci.plugins.dockerbuildstep.action.EnvInvisibleAction;
+import org.jenkinsci.plugins.dockerbuildstep.log.ConsoleLogger;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import com.kpelykh.docker.client.DockerClient;
+import com.kpelykh.docker.client.DockerException;
+import com.kpelykh.docker.client.model.Container;
+import com.kpelykh.docker.client.model.ContainerInspectResponse;
+
+public class StartByImageIdCommand extends DockerCommand {
+
+    private String imageId;
+
+    @DataBoundConstructor
+    public StartByImageIdCommand(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    @Override
+    public void execute(@SuppressWarnings("rawtypes") AbstractBuild build, ConsoleLogger console)
+            throws DockerException {
+        if (imageId == null || imageId.isEmpty()) {
+            throw new IllegalArgumentException("At least one parameter is required");
+        }
+
+        DockerClient client = getClient();
+        List<Container> containers = getClient().listContainers(true);
+        for (Container c : containers) {
+            if (imageId.equalsIgnoreCase(c.getImage())) {
+                client.startContainer(c.getId());
+                console.logInfo("started container id " + c.getId());
+
+                ContainerInspectResponse inspectResp = client.inspectContainer(c.getId());
+                EnvInvisibleAction envAction = new EnvInvisibleAction(inspectResp);
+                build.addAction(envAction);
+            }
+        }
+    }
+
+    @Extension
+    public static class StartCommandDescriptor extends DockerCommandDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Start constainer(s) by image ID";
+        }
+    }
+
+}
