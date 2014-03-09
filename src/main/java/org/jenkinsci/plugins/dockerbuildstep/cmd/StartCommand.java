aa0a919c62423da4a1337f31bd349e1f84389b84
Vojtech Juranek <vjuranek@redhat.com>
2014-03-09 22:59:55 +0100

    Add option to wait for ports when starting containers

--- src/main/java/org/jenkinsci/plugins/dockerbuildstep/cmd/StartCommand.java
+++ src/main/java/org/jenkinsci/plugins/dockerbuildstep/cmd/StartCommand.java
@@ -5,9 +5,11 @@ import hudson.model.AbstractBuild;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.jenkinsci.plugins.dockerbuildstep.action.EnvInvisibleAction;
 import org.jenkinsci.plugins.dockerbuildstep.log.ConsoleLogger;
+import org.jenkinsci.plugins.dockerbuildstep.util.PortUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import com.kpelykh.docker.client.DockerClient;
@@ -26,14 +28,20 @@ import com.kpelykh.docker.client.model.ContainerInspectResponse;
 public class StartCommand extends DockerCommand {
 
     private String containerIds;
+    private final String waitPorts;
 
     @DataBoundConstructor
-    public StartCommand(String containerIds) {
+    public StartCommand(String containerIds, String waitPorts) {
         this.containerIds = containerIds;
+        this.waitPorts = waitPorts;
     }
 
     public String getContainerIds() {
         return containerIds;
+    }
+    
+    public String getWaitPorts() {
+        return waitPorts;
     }
 
     @Override
@@ -54,6 +62,21 @@ public class StartCommand extends DockerCommand {
             ContainerInspectResponse inspectResp = client.inspectContainer(id);
             EnvInvisibleAction envAction = new EnvInvisibleAction(inspectResp);
             build.addAction(envAction);
+        }
+        
+        //wait for ports
+        if(waitPorts != null && !waitPorts.isEmpty()) {
+            Map<String, List<Integer>> containers = PortUtils.parsePorts(waitPorts);
+            for(String cId : containers.keySet()) {
+                ContainerInspectResponse inspectResp = client.inspectContainer(cId);
+                String ip = inspectResp.getNetworkSettings().ipAddress;
+                List<Integer> ports = containers.get(cId);
+                for(Integer port : ports) {
+                    System.out.println("Waiting for port " + port + " on " + ip);
+                    boolean isOk = PortUtils.waitForPort(ip, port);
+                    System.out.println("Wait for port went " + isOk);
+                }
+            }
         }
     }
 
