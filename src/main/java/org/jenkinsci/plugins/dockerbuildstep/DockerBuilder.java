3f73a8726811ab9987d4eb4702d4294d92d96b69
Vojtech Juranek <vjuranek@redhat.com>
2015-01-20 21:49:16 +0100

    Adjust indentation

--- src/main/java/org/jenkinsci/plugins/dockerbuildstep/DockerBuilder.java
+++ src/main/java/org/jenkinsci/plugins/dockerbuildstep/DockerBuilder.java
@@ -48,141 +48,141 @@ import com.github.dockerjava.api.DockerException;
  */
 public class DockerBuilder extends Builder {
 
-    private DockerCommand dockerCmd;
+	private DockerCommand dockerCmd;
 
-    @DataBoundConstructor
-    public DockerBuilder(DockerCommand dockerCmd) {
-        this.dockerCmd = dockerCmd;
-    }
+	@DataBoundConstructor
+	public DockerBuilder(DockerCommand dockerCmd) {
+		this.dockerCmd = dockerCmd;
+	}
 
-    public DockerCommand getDockerCmd() {
-        return dockerCmd;
-    }
+	public DockerCommand getDockerCmd() {
+		return dockerCmd;
+	}
 
-    @Override
-    public boolean perform(@SuppressWarnings("rawtypes") AbstractBuild build, Launcher launcher, BuildListener listener)
-            throws AbortException {
+	@Override
+	public boolean perform(@SuppressWarnings("rawtypes") AbstractBuild build, Launcher launcher, BuildListener listener)
+			throws AbortException {
 
-        ConsoleLogger clog = new ConsoleLogger(listener);
+		ConsoleLogger clog = new ConsoleLogger(listener);
 
-        if (getDescriptor().getDockerClient() == null) {
-            clog.logError("docker client is not initialized, command '" + dockerCmd.getDescriptor().getDisplayName()
-                    + "' was aborted. Check Jenkins server log which Docker client wasn't initialized");
-            throw new AbortException("Docker client wasn't initialized.");
-        }
+		if (getDescriptor().getDockerClient() == null) {
+			clog.logError("docker client is not initialized, command '" + dockerCmd.getDescriptor().getDisplayName()
+					+ "' was aborted. Check Jenkins server log which Docker client wasn't initialized");
+			throw new AbortException("Docker client wasn't initialized.");
+		}
 
-        try {
-            dockerCmd.execute(build, clog);
-        } catch (DockerException e) {
-            clog.logError("command '" + dockerCmd.getDescriptor().getDisplayName() + "' failed: " + e.getMessage());
-            LOGGER.severe("Failed to execute Docker command " + dockerCmd.getDescriptor().getDisplayName() + ": "
-                    + e.getMessage());
-            throw new AbortException(e.getMessage());
-        }
-        return true;
-    }
+		try {
+			dockerCmd.execute(build, clog);
+		} catch (DockerException e) {
+			clog.logError("command '" + dockerCmd.getDescriptor().getDisplayName() + "' failed: " + e.getMessage());
+			LOGGER.severe("Failed to execute Docker command " + dockerCmd.getDescriptor().getDisplayName() + ": "
+					+ e.getMessage());
+			throw new AbortException(e.getMessage());
+		}
+		return true;
+	}
 
-    @Override
-    public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl) super.getDescriptor();
-    }
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
 
-    @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
-        private String dockerUrl;
-        private String dockerVersion;
-        private transient DockerClient dockerClient;
+		private String dockerUrl;
+		private String dockerVersion;
+		private transient DockerClient dockerClient;
 
-        public DescriptorImpl() {
-            load();
+		public DescriptorImpl() {
+			load();
 
-            if (isEmpty(dockerUrl)) {
-                LOGGER.warning("Docker URL is not set, docker client won't be initialized");
-                return;
-            }
+			if (isEmpty(dockerUrl)) {
+				LOGGER.warning("Docker URL is not set, docker client won't be initialized");
+				return;
+			}
 
-            try {
-                dockerClient = createDockerClient(dockerUrl, dockerVersion);
-            } catch (DockerException e) {
-                LOGGER.warning("Cannot create Docker client: " + e.getCause());
-            }
-        }
+			try {
+				dockerClient = createDockerClient(dockerUrl, dockerVersion);
+			} catch (DockerException e) {
+				LOGGER.warning("Cannot create Docker client: " + e.getCause());
+			}
+		}
 
-        private static DockerClient createDockerClient(String dockerUrl, String dockerVersion) {
-        	//TODO JENKINS-26512
-        	SSLConfig dummySSLConf = (new SSLConfig() {	
+		private static DockerClient createDockerClient(String dockerUrl, String dockerVersion) {
+			// TODO JENKINS-26512
+			SSLConfig dummySSLConf = (new SSLConfig() {
 				public SSLContext getSSLContext() throws KeyManagementException, UnrecoverableKeyException,
 						NoSuchAlgorithmException, KeyStoreException {
 					return null;
 				}
 			});
-        	
-            DockerClientConfigBuilder configBuilder = new DockerClientConfigBuilder()
-                    .withUri(dockerUrl).withVersion(dockerVersion).withSSLConfig(dummySSLConf);
-            ClassLoader classLoader = Jenkins.getInstance().getPluginManager().uberClassLoader;
-            return DockerClientBuilder.getInstance(configBuilder)
-                    .withServiceLoaderClassLoader(classLoader).build();
-        }
 
-        public FormValidation doTestConnection(@QueryParameter String dockerUrl, @QueryParameter String dockerVersion) throws IOException, ServletException {
-            LOGGER.fine(String.format("Trying to get client for %s and version %s", dockerUrl, dockerVersion));
-            try {
-                DockerClient dockerClient = createDockerClient(dockerUrl, dockerVersion);
-                dockerClient.pingCmd().exec();
-            } catch (Exception e) {
-                LOGGER.log(Level.WARNING, e.getMessage(), e);
-                return FormValidation.error("Something went wrong, cannot connect to " + dockerUrl + ", cause: "
-                        + e.getCause());
-            }
-            return FormValidation.ok("Connected to " + dockerUrl);
-        }
+			DockerClientConfigBuilder configBuilder = new DockerClientConfigBuilder().withUri(dockerUrl)
+					.withVersion(dockerVersion).withSSLConfig(dummySSLConf);
+			ClassLoader classLoader = Jenkins.getInstance().getPluginManager().uberClassLoader;
+			return DockerClientBuilder.getInstance(configBuilder).withServiceLoaderClassLoader(classLoader).build();
+		}
 
-        public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> aClass) {
-            return true;
-        }
+		public FormValidation doTestConnection(@QueryParameter String dockerUrl, @QueryParameter String dockerVersion)
+				throws IOException, ServletException {
+			LOGGER.fine(String.format("Trying to get client for %s and version %s", dockerUrl, dockerVersion));
+			try {
+				DockerClient dockerClient = createDockerClient(dockerUrl, dockerVersion);
+				dockerClient.pingCmd().exec();
+			} catch (Exception e) {
+				LOGGER.log(Level.WARNING, e.getMessage(), e);
+				return FormValidation.error("Something went wrong, cannot connect to " + dockerUrl + ", cause: "
+						+ e.getCause());
+			}
+			return FormValidation.ok("Connected to " + dockerUrl);
+		}
 
-        public String getDisplayName() {
-            return "Execute Docker container";
-        }
+		public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> aClass) {
+			return true;
+		}
 
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            dockerUrl = formData.getString("dockerUrl");
-            dockerVersion = formData.getString("dockerVersion");
-            if (isBlank(dockerUrl)) {
-                LOGGER.severe("Docker URL is empty, Docker build test plugin cannot work without Docker URL being set up properly");
-                //JENKINS-23733 doen't block user to save the config if admin decides so
-                return true;
-            }
+		public String getDisplayName() {
+			return "Execute Docker container";
+		}
 
-            save();
-            try {
-                dockerClient = createDockerClient(dockerUrl, dockerVersion);
-            } catch (DockerException e) {
-                LOGGER.warning("Cannot create Docker client: " + e.getCause());
-            }
-            return super.configure(req, formData);
-        }
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+			dockerUrl = formData.getString("dockerUrl");
+			dockerVersion = formData.getString("dockerVersion");
+			if (isBlank(dockerUrl)) {
+				LOGGER.severe("Docker URL is empty, Docker build test plugin cannot work without Docker URL being set up properly");
+				// JENKINS-23733 doen't block user to save the config if admin decides so
+				return true;
+			}
 
-        public String getDockerUrl() {
-            return dockerUrl;
-        }
-        
-        public String getDockerVersion() {
-            return dockerVersion;
-        }
+			save();
+			try {
+				dockerClient = createDockerClient(dockerUrl, dockerVersion);
+			} catch (DockerException e) {
+				LOGGER.warning("Cannot create Docker client: " + e.getCause());
+			}
+			return super.configure(req, formData);
+		}
 
-        public DockerClient getDockerClient() {
-            return dockerClient;
-        }
+		public String getDockerUrl() {
+			return dockerUrl;
+		}
 
-        public DescriptorExtensionList<DockerCommand, DockerCommandDescriptor> getCmdDescriptors() {
-            return DockerCommand.all();
-        }
-        
-    }
+		public String getDockerVersion() {
+			return dockerVersion;
+		}
 
-    private static Logger LOGGER = Logger.getLogger(DockerBuilder.class.getName());
+		public DockerClient getDockerClient() {
+			return dockerClient;
+		}
+
+		public DescriptorExtensionList<DockerCommand, DockerCommandDescriptor> getCmdDescriptors() {
+			return DockerCommand.all();
+		}
+
+	}
+
+	private static Logger LOGGER = Logger.getLogger(DockerBuilder.class.getName());
 
 }
