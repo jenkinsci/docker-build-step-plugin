c8dd3cb26e6009a7aa7c40818d3ad9e3b224a99d
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-03-26 17:05:59 +0100

    Migrate tests to JUnit Jupiter
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup
    * Ban JUnit4 imports

--- src/test/java/org/jenkinsci/plugins/dockerbuildstep/util/PortBindingParserTest.java
+++ src/test/java/org/jenkinsci/plugins/dockerbuildstep/util/PortBindingParserTest.java
@@ -1,108 +1,109 @@
 package org.jenkinsci.plugins.dockerbuildstep.util;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 
+import org.junit.jupiter.api.Test;
+
 /**
  *  Defines legal syntax for entering port bindings.
  */
-public class PortBindingParserTest {
+class PortBindingParserTest {
 
     private static final ExposedPort TCP_8080 = ExposedPort.tcp(8080);
 
     @Test
-    public void fullDefinition_blank() {
+    void fullDefinition_blank() {
         assertCreatesBindings("127.0.0.1:80 8080/tcp",
                 new PortBinding(Ports.Binding.bindIpAndPort("127.0.0.1", 80), TCP_8080));
     }
-    
+
     @Test
-    public void noProtocol_blank() {
+    void noProtocol_blank() {
         assertCreatesBindings("127.0.0.1:80 8080",
                 new PortBinding(Ports.Binding.bindIpAndPort("127.0.0.1", 80), TCP_8080));
     }
-    
+
     @Test
-    public void noHostIp_blank() {
+    void noHostIp_blank() {
         assertCreatesBindings("80 8080/tcp",
                 new PortBinding(Ports.Binding.bindPort(80), TCP_8080));
     }
-    
+
     @Test
-    public void portsOnly_blank() {
+    void portsOnly_blank() {
         assertCreatesBindings("80 8080",
                 new PortBinding(Ports.Binding.bindPort(80), TCP_8080));
     }
-    
+
     @Test
-    public void fullDefinition_colon() {
+    void fullDefinition_colon() {
         assertCreatesBindings("127.0.0.1:80:8080/tcp",
                 new PortBinding(Ports.Binding.bindIpAndPort("127.0.0.1", 80), TCP_8080));
     }
-    
+
     @Test
-    public void noProtocol_colon() {
+    void noProtocol_colon() {
         assertCreatesBindings("127.0.0.1:80:8080",
                 new PortBinding(Ports.Binding.bindIpAndPort("127.0.0.1", 80), TCP_8080));
     }
-    
+
     @Test
-    public void noHostIp_colon() {
+    void noHostIp_colon() {
         assertCreatesBindings("80:8080/tcp",
                 new PortBinding(Ports.Binding.bindPort(80), TCP_8080));
     }
-    
+
     @Test
-    public void portsOnly_colon() {
+    void portsOnly_colon() {
         assertCreatesBindings("80:8080",
                 new PortBinding(Ports.Binding.bindPort(80), TCP_8080));
     }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void syntaxError() {
-        PortBindingParser.parse("nonsense");
-    }
-    
+
     @Test
-    public void parseEmptyString() {
+    void syntaxError() {
+        assertThrows(IllegalArgumentException.class, () ->
+            PortBindingParser.parse("nonsense"));
+    }
+
+    @Test
+    void parseEmptyString() {
         PortBinding[] bindings = PortBindingParser.parse("");
         assertEquals(0, bindings.length);
     }
-    
+
     @Test
-    public void exposedUdpPort() {
+    void exposedUdpPort() {
         assertCreatesBindings("80 8080/udp",
                 new PortBinding(Ports.Binding.bindPort(80), ExposedPort.udp(8080)));
     }
-    
+
     @Test
-    public void dynamicHostPort_blank() {
+    void dynamicHostPort_blank() {
         assertCreatesBindings("127.0.0.1: 8080",
                 new PortBinding(Ports.Binding.bindIp("127.0.0.1"), TCP_8080));
     }
-    
+
     @Test
-    public void dynamicHostPort_colon() {
+    void dynamicHostPort_colon() {
         assertCreatesBindings("127.0.0.1::8080",
                 new PortBinding(Ports.Binding.bindIp("127.0.0.1"), TCP_8080));
     }
-    
+
     @Test
-    public void twoBindings_UnixStyle() {
+    void twoBindings_UnixStyle() {
         twoBindings("80 8080\n81 8081");
     }
-    
+
     @Test
-    public void twoBindings_DosStyle() {
+    void twoBindings_DosStyle() {
         twoBindings("80 8080\r\n81 8081");
     }
     
-    private void twoBindings(String input) {
+    private static void twoBindings(String input) {
         assertCreatesBindings(input,
                 new PortBinding(Ports.Binding.bindPort(80), ExposedPort.tcp(8080)),
                 new PortBinding(Ports.Binding.bindPort(81), ExposedPort.tcp(8081)));
@@ -110,7 +111,7 @@ public class PortBindingParserTest {
     
     private static void assertCreatesBindings(String input, PortBinding... expected) {
         PortBinding[] parsed = PortBindingParser.parse(input);
-        assertEquals("wrong number of PortBindings created", expected.length, parsed.length);
+        assertEquals(expected.length, parsed.length, "wrong number of PortBindings created");
         
         for (int i = 0; i < parsed.length; i++) {
             assertEquals(expected[i], parsed[i]);
