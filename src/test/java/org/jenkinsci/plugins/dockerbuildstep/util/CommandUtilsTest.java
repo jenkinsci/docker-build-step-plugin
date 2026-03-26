02668b99ee1d8dda0ebf589ac159811c03d169a4
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-03-26 17:05:59 +0100

    Migrate tests to JUnit Jupiter
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup
    * Ban JUnit4 imports

--- src/test/java/org/jenkinsci/plugins/dockerbuildstep/util/CommandUtilsTest.java
+++ src/test/java/org/jenkinsci/plugins/dockerbuildstep/util/CommandUtilsTest.java
@@ -1,18 +1,18 @@
 package org.jenkinsci.plugins.dockerbuildstep.util;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Class that tests CommandUtils
  *
  * @author wzheng2310@gmail.com (Wei Zheng)
  */
-public class CommandUtilsTest {
+class CommandUtilsTest {
 
     @Test
-    public void testImageFullNameFrom() {
+    void testImageFullNameFrom() {
         assertEquals("img", CommandUtils.imageFullNameFrom("", "img", ""));
         assertEquals("img:tag", CommandUtils.imageFullNameFrom("", "img", "tag"));
         assertEquals("repo/img", CommandUtils.imageFullNameFrom("", "repo/img", ""));
@@ -22,7 +22,7 @@ public class CommandUtilsTest {
     }
 
     @Test
-    public void testAddLatestTagIfNeeded() {
+    void testAddLatestTagIfNeeded() {
         String[] input = {
                 "img", "img:1", "img:123", "img:latest",
                 "repo/img", "repo/img:1", "repo/img:123", "repo/img:latest",
@@ -36,21 +36,21 @@ public class CommandUtilsTest {
                 "reg:80/repo/img:latest", "reg:80/repo/img:1", "reg:80/repo/img:123", "reg:80/repo/img:latest"
             };
 
-        assertEquals("input length and output length differ!", input.length, output.length);
+        assertEquals(input.length, output.length, "input length and output length differ!");
         for (int i = 0; i < input.length; i++ ) {
             assertEquals(output[i], CommandUtils.addLatestTagIfNeeded(input[i]));
         }
     }
-    
+
     @Test
-    public void testSizeInBytes() {
+    void testSizeInBytes() {
     	String[] input = {
     			"64", "128b", "256k", "512m", "1g", "666a", "-9mb" 
     	};
     	long[] output = {
     			64, 128, 262144, 536870912, 1073741824, -1, -1
     	};
-        assertEquals("input length and output length differ!", input.length, output.length);
+        assertEquals(input.length, output.length, "input length and output length differ!");
         for (int i = 0; i < input.length; i++ ) {
             assertEquals(output[i], CommandUtils.sizeInBytes(input[i]));
         }
